42699da4e106355ba2f4d51d4e8627a5d6ebb091
Recai <rebehzat@users.noreply.github.com>
2026-09-25 13:16:44 +0300

    Add PeerTube video search engine

---
+++ src/engines/catalog/peertube.rs
@@ -0,0 +1,9 @@
+pub fn definition() -> crate::engines::catalog::CatalogEntry {
+    crate::engine_catalog_entry!("peertube", "json_api", enabled = false, [
+        "endpoint" => "https://peer.tube/api/v1/search/videos?searchTarget=search-index&resultType=videos&start=0&count=10&sort=-match&nsfw=both",
+        "query_param" => "search",
+        "results_path" => "data",
+        "title_field" => "name",
+        "url_field" => "url",
+    ])
+}
