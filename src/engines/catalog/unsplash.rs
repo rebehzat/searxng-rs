38d636cc3dcc9312ea15e6c7dbc96e529cba37cd
Recai <rebehzat@users.noreply.github.com>
2026-09-25 14:17:31 +0300

    Add Unsplash image search engine

---
+++ src/engines/catalog/unsplash.rs
@@ -0,0 +1,10 @@
+pub fn definition() -> crate::engines::catalog::CatalogEntry {
+    crate::engine_catalog_entry!("unsplash", "json_api", [
+        "endpoint" => "https://unsplash.com/napi/search/photos?page=1&per_page=20",
+        "query_param" => "query",
+        "results_path" => "results",
+        "title_field" => "alt_description",
+        "url_field" => "links.html",
+        "snippet_field" => "description",
+    ])
+}
