c5fae210181e9d0643c47e1639a7926211a3d510
Recai Mete BIÇAKCI <foxxytux420@gmail.com>
2026-09-25 19:43:01 +0300

    Enable unsplash by default (#81)
    Co-authored-by: Recai <rebehzat@users.noreply.github.com>

--- src/engines/catalog/unsplash.rs
+++ src/engines/catalog/unsplash.rs
@@ -1,5 +1,5 @@
 pub fn definition() -> crate::engines::catalog::CatalogEntry {
-    crate::engine_catalog_entry!("unsplash", "json_api", enabled = false, [
+    crate::engine_catalog_entry!("unsplash", "json_api", [
         "endpoint" => "https://unsplash.com/napi/search/photos?page=1&per_page=20",
         "query_param" => "query",
         "results_path" => "results",
